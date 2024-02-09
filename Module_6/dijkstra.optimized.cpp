9dd7190eadcbb9ce07931ce49b55d99a00ef3948
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-02-09 06:05:06 +0600

    Module 6

---
+++ Module_6/dijkstra.optimized.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 100;
+vector<pair<int, int>> adj[N];
+int dis[N]; // distance
+class cmp
+{
+public:
+    bool operator()(pair<int, int> a, pair<int, int> b)
+    {
+        return a.second > b.second;
+    }
+};
+void dijkstra(int src)
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
+    pq.push({src, 0});
+    dis[src] = 0;
+    while (!pq.empty())
+    {
+        pair<int, int> parent = pq.top();
+        pq.pop();
+        int parentNode = parent.first;
+        int parentCost = parent.second;
+        for (pair<int, int> child : adj[parentNode])
+        {
+            int childNode = child.first;
+            int childCost = child.second;
+            if (parentCost + childCost < dis[childNode])
+            {
+                // path relax
+                dis[childNode] = parentCost + childCost;
+                pq.push({childNode, dis[childNode]});
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj[a].push_back({b, c});
+        adj[b].push_back({a, c});
+    }
+    for (int i = 0; i < n; i++)
+    {
+        dis[i] = INT_MAX;
+    }
+    dijkstra(0);
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << "-> " << dis[i] << endl;
+    }
+    return 0;
+}
