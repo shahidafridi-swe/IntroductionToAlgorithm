b4bc7080eef42a51cab7e13ec8079b2efd0019bf
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-02-20 21:09:53 +0600

    M-10

--- Module_10/dsu.cpp
+++ Module_10/dsu.cpp
@@ -3,34 +3,75 @@ using namespace std;
 
 const int N = 1e5 + 5;
 int parent[N];
-
+int group_size[N];
+int level[N];
 void dsu_initialize(int n)
 {
     for (int i = 0; i < n; i++)
     {
         parent[i] = -1;
+        group_size[i] = 1;
+        level[i] = 0;
     }
-    parent[1] = 3;
-    parent[2] = 1;
 }
 
-int find(int node)
+int dsu_find(int node)
 {
     if (parent[node] == -1)
         return node;
-    int leader = find(parent[node]);
+    int leader = dsu_find(parent[node]);
     parent[node] = leader;
     return leader;
 }
-
+void dsu_union(int node1, int node2)
+{
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+    parent[leaderA] = leaderB;
+}
+void dsu_union_by_level(int node1, int node2)
+{
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+    if (level[leaderA] > level[leaderB])
+    {
+        parent[leaderB] = leaderA;
+    }
+    else if (level[leaderB] > level[leaderA])
+    {
+        parent[leaderA] = leaderB;
+    }
+    else
+    {
+        parent[leaderA] = leaderB;
+        level[leaderB]++;
+    }
+}
+void dsu_union_by_size(int node1, int node2)
+{
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+    if (group_size[leaderA] > group_size[leaderB])
+    {
+        parent[leaderB] = leaderA;
+        group_size[leaderA] += group_size[leaderB];
+    }
+    else
+    {
+        parent[leaderA] = leaderB;
+        group_size[leaderB] += group_size[leaderA];
+    }
+}
 int main()
 {
-    dsu_initialize(5);
-    cout << find(0) << endl;
-    cout << find(1) << endl;
-    cout << find(2) << endl;
-    cout << find(3) << endl;
-    cout << find(4) << endl;
+    dsu_initialize(7);
+    dsu_union_by_level(1, 2);
+    dsu_union_by_level(2, 3);
+    dsu_union_by_level(4, 5);
+    dsu_union_by_level(5, 6);
+    dsu_union_by_level(1, 4);
+    cout << dsu_find(3) << endl;
+    cout << dsu_find(6) << endl;
 
     return 0;
 }
