d9e0ef228aa602673375cfc27af1bb44b253fac6
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-02-28 23:34:53 +0600

    m-17

---
+++ Module_17/fibonacci_with_dp.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll const N = 1e6 + 5;
+ll dp[N];
+
+ll fibo(ll n)
+{
+    if (n == 0 || n == 1)
+        return n;
+    if (dp[n] != -1)
+        return dp[n];
+    ll ans = fibo(n - 1) + fibo(n - 2);
+    dp[n] = ans;
+    return ans;
+}
+
+int main()
+{
+    ll n;
+    cin >> n;
+    memset(dp, -1, sizeof(dp));
+    cout << fibo(n);
+    return 0;
+}
