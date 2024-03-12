8622c478ec9e58b88ed7f9563514cc49e9fde8f6
shahidafridi-swe <shahidafridi.cse@gmail.com>
2024-03-12 20:17:37 +0600

    M-19

---
+++ Module_19/Count_Subset_Sum_with_Given_Difference.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    int s = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        s += a[i];
+    }
+    int dif;
+    cin >> dif;
+    int sum;
+
+    sum = (s + dif) / 2;
+    int dp[n + 1][sum + 1];
+    dp[0][0] = 1;
+    for (int i = 1; i <= sum; i++)
+    {
+        dp[0][i] = 0;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 0; j <= sum; j++)
+        {
+            if (a[i - 1] <= j)
+            {
+               
+                dp[i][j] = dp[i - 1][j - a[i - 1]] + dp[i - 1][j];
+            }
+            else
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+    }
+
+    cout << dp[n][sum];
+    return 0;
+}
